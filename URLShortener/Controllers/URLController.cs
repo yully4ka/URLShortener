f38942aed3b0f903a8485f0af4b2e7b806840827
yully4ka <cornea.iulia@gmail.com>
2017-07-27 23:17:24 +0300

    Second commit

---
+++ URLShortener/Controllers/URLController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using URLShortener.Models;
+
+namespace URLShortener.Controllers
+{
+    public class URLController : Controller
+    {
+        // GET: URL
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Url url = new Url();
+            return View(url);
+        }
+
+        public ActionResult Index(Url url)
+        {
+            if (ModelState.IsValid)
+            {
+                url.ShortURL = "http://www.google.com";
+            }
+            return View(url);
+        }
+    }
+}
---
+++ URLShortener/Models/Url.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace URLShortener.Models
+{
+    public class Url
+    {
+        [Required]
+        public string LongURL { get; set; }
+        public string ShortURL { get; set; }
+    }
+}
